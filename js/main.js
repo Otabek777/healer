aa0c64357ada60e268e5ef9e43aa37923aa362ca
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-04-29 15:22:14 +0500

    mobile

--- js/main.js
+++ js/main.js
@@ -15,6 +15,15 @@ if(document.querySelector('.tabs')) {
         });
     })(jQuery);
 }
+$( ".mobile_header__burgir" ).click(function() {
+    $(this).toggleClass('active');
+    $('.header').toggleClass('active');
+    $('body').toggleClass('hidden');
+});
+$( ".modal__close" ).click(function() {
+    $('.modal').removeClass('active');
+    $('body').removeClass('hidden');
+});
 if(document.querySelector('.read_more_block')) {
     $( ".open_read_more" ).click(function() {
         $(this).addClass('none');
@@ -56,4 +65,60 @@ if(document.querySelector('.reviews__slick')) {
 };
 
 
-
+// SELECT
+if(document.querySelector('.select')) {
+    $('.select').each(function() {
+        const _this = $(this),
+            selectOption = _this.find('option'),
+            selectOptionLength = selectOption.length,
+            selectedOption = selectOption.filter(':selected'),
+            duration = 450; // длительность анимации 
+    
+        _this.hide();
+        _this.wrap('<div class="select"></div>');
+        $('<div>', {
+            class: 'new-select',
+            text: _this.children('option:disabled').text()
+        }).insertAfter(_this);
+    
+        const selectHead = _this.next('.new-select');
+        $('<div>', {
+            class: 'new-select__list'
+        }).insertAfter(selectHead);
+    
+        const selectList = selectHead.next('.new-select__list');
+        for (let i = 1; i < selectOptionLength; i++) {
+            $('<div>', {
+                class: 'new-select__item',
+                html: $('<span>', {
+                    text: selectOption.eq(i).text()
+                })
+            })
+            .attr('data-value', selectOption.eq(i).val())
+            .appendTo(selectList);
+        }
+    
+        const selectItem = selectList.find('.new-select__item');
+        selectList.slideUp(0);
+        selectHead.on('click', function() {
+            if ( !$(this).hasClass('on') ) {
+                $(this).addClass('on');
+                selectList.slideDown(duration);
+    
+                selectItem.on('click', function() {
+                    let chooseItem = $(this).data('value');
+    
+                    $('select').val(chooseItem).attr('selected', 'selected');
+                    selectHead.text( $(this).find('span').text() );
+    
+                    selectList.slideUp(duration);
+                    selectHead.removeClass('on');
+                });
+    
+            } else {
+                $(this).removeClass('on');
+                selectList.slideUp(duration);
+            }
+        });
+    });
+}
